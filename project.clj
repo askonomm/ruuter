601011103e7698fbbd3f21f44c3e9ecc59d50e39
Asko Nõmm <asko@bien.ee>
2021-10-02 01:42:19 -0300

    bump

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject ruuter "1.0.0"
+(defproject org.clojars.askonomm/ruuter "1.0.0"
   :description "A tiny HTTP router"
   :url "https://github.com/askonomm/ruuter"
   :license {:name "MIT"
             :url  "https://raw.githubusercontent.com/askonomm/ruuter/master/LICENSE.txt"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [http-kit "2.5.3"]]
   :main ruuter.core
   :min-lein-version "2.0.0"
